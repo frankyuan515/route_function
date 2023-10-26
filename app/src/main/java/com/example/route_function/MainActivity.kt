cd2ae01aaa0875420301867074b7f45686bca8a5
frankyuan515 <71625128+frankyuan515@users.noreply.github.com>
2023-10-26 09:04:29 +0200

    update: inputs bars

--- app/src/main/java/com/example/route_function/MainActivity.kt
+++ app/src/main/java/com/example/route_function/MainActivity.kt
@@ -25,17 +25,16 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontVariation.weight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.route_function.ui.theme.Route_functionTheme
-//
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Mapbox.getInstance(applicationContext, "YOUR_MAPBOX_ACCESS_TOKEN")
         setContent {
             Route_functionTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -47,43 +46,36 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun HarborRoutingScreen() {
-    var longitude by remember { mutableStateOf("") }
-    var latitude by remember { mutableStateOf("") }
-
+    var startHarbor by remember { mutableStateOf("") }
+    var endHarbor by remember { mutableStateOf("") }
     var displayResults by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomCenter // Align the content at the bottom of the screen
+        contentAlignment = Alignment.TopCenter
     ) {
-        // Display user inputs in the second Box
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.Gray) // Background color for the map
+                .background(Color.Gray)
         ) {
-            // Display user inputs and results
             Column(
                 modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
-
             ) {
-                // Display longitude and latitude entered by the user
-                Text("Longitude: $longitude")
-                Text("Latitude: $latitude")
+                Text("Start Harbor: $startHarbor")
+                Text("End Harbor: $endHarbor")
 
                 if (displayResults) {
-                    // Display the results when the button is clicked
-                    Text("Result: Longitude - $longitude, Latitude - $latitude")
 
+
+                    Text("Result: Start Harbor - $startHarbor, End Harbor - $endHarbor")
                 }
             }
         }
 
-        // Input fields and button
         Column(
             modifier = Modifier
                 .padding(16.dp)
@@ -95,47 +87,48 @@ fun HarborRoutingScreen() {
                 .padding(16.dp)
                 .height(IntrinsicSize.Min),
             verticalArrangement = Arrangement.spacedBy(16.dp)
-
         ) {
             BasicTextField(
-                value = longitude,
-                onValueChange = { text: String -> longitude = text },
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                value = startHarbor,
+                onValueChange = { text: String -> startHarbor = text },
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+//                //keyboardOptions = KeyboardOptions(
+//                    keyboardType = KeyboardType.Number,
+//                    imeAction = ImeAction.Next
+//                    ),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.LightGray) // Change the background color here
-
+                    .background(Color.LightGray)
+                    .padding(8.dp)
             )
 
             BasicTextField(
-                value = latitude,
-                onValueChange = { text: String -> latitude = text },
+                value = endHarbor,
+                onValueChange = { text: String -> endHarbor = text },
                 keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.LightGray) // Change the background color here
-
+                    .background(Color.LightGray)
+                    .padding(8.dp)
             )
 
             Button(
                 onClick = {
-                    // Handle the button click, e.g., trigger the routing process.
                     displayResults = true
                 }
             ) {
-                Text(text = "Create Route")
+                Text(text = "SEARCH Route")
             }
         }
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
 fun HarborRoutingScreenPreview() {
     Route_functionTheme {
-        var longitude by remember { mutableStateOf("") }
-        var latitude by remember { mutableStateOf("") }
+        var startHarbor by remember { mutableStateOf("") }
+        var endHarbor by remember { mutableStateOf("") }
         var displayResults by remember { mutableStateOf(false) }
 
         Surface(
